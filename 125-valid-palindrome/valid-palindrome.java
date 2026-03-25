efa6f301b9461d698293d7bd3faf0ac004c13255
Abdul Kabeer <vh12723_cse23@velhightech.com>
2026-03-25 20:17:39 +0530

    Time: 4 ms (49.36%) | Memory: 45.1 MB (41.75%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,19 +1,13 @@
 class Solution {
-    public static String reverse(String result){
-        String ch = "";
-        for(int i=result.length()-1;i>=0;i--){
-             ch = ch + result.charAt(i);
-        }
-        return ch;
-    }
     public boolean isPalindrome(String s) {
-        String result = "";
+        StringBuilder res = new StringBuilder();
         for(int i=0;i<s.length();i++){
             if(Character.isLetterOrDigit(s.charAt(i))){
-                result = result+s.charAt(i);
-            } 
-        }result = result.toLowerCase();
-        String reversed = reverse(result);
-        return reversed.equals(result);
+                res.append(Character.toLowerCase(s.charAt(i)));
+            }
+         
+        }   String original = res.toString();
+          String  reversed = new StringBuilder(original).reverse().toString();
+          return original.equals(reversed);
     }
 }
